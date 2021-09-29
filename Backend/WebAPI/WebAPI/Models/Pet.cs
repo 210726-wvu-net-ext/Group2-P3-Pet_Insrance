fea19a5a2ac8ba4081004399ebd094f4c9dfb58d
Dekklin <collinhintzke@outlook.com>
2021-09-29 14:54:50 -0700

    Added a whole lot of scaffolding to get base Hello World

---
+++ Backend/WebAPI/WebAPI/Models/Pet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Breed { get; set; }
+        public decimal Age { get; set; }
+        public string Location { get; set; }
+        public string InsurancePlan { get; set; }
+        public string InsuranceMonthly { get; set; }
+        public int? UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ Backend/WebAPI/WebAPI/Repositories/UserRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+using WebAPI.Repositories.Interfaces;
+
+namespace WebAPI.Repositories
+{
+    public class UserRepository : IUser
+    {
+      
+    }
+}
