828c91440c6d4800bb52f0069e259fe092f71e58
Dylan Restivo <dylan.restivo@yahoo.com>
2021-10-08 16:46:53 -0500

    First pass PR tests

--- WebAPI/APITesting/RepositoriesTests/PetRepoTests.cs
+++ WebAPI/APITesting/RepositoriesTests/PetRepoTests.cs
@@ -3,6 +3,7 @@ using WebAPI.Repositories;
 using Moq;
 using WebAPI.Models;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace APITesting.RepositoriesTests
 {
@@ -15,7 +16,6 @@ namespace APITesting.RepositoriesTests
             _context = new Mock<WebAPI.Entities.petinsuranceContext>();
         }
 
-
         Pet testPet = new()
         {
             Breed = "Crested Gecko",
@@ -23,7 +23,7 @@ namespace APITesting.RepositoriesTests
             Location = "Hawaii",
             InsurancePlan = "Gold",
             InsuranceMonthly = "Gold",
-            UserId = 1
+            UserId = 5,
         };
 
         /// <summary>
@@ -35,11 +35,94 @@ namespace APITesting.RepositoriesTests
         {
 
             var _petRepo = new PetRepository(_context.Object);
-            var result = _petRepo.AddPet(testPet as Pet);
+            var result = _petRepo.AddPet(testPet);
 
             Assert.IsAssignableFrom<Task>(result);
         }
 
+        /// <summary>
+        /// Tests DeletePetAsync to check if result is type Task
+        /// </summary>
+
+        [Fact]
+        public void DeletePetAsync_TestIfType_Task()
+        {
+
+            var _petRepo = new PetRepository(_context.Object);
+            var result = _petRepo.DeletePetAsync(testPet);
+
+            Assert.IsAssignableFrom<Task>(result);
+        }
+
+        /// <summary>
+        /// Tests GetPetById to check if result is type Task
+        /// </summary>
+
+        [Fact]
+        public void GetPetById_TestIfType_Task()
+        {
+
+            var _petRepo = new PetRepository(_context.Object);
+            var result = _petRepo.GetPetById(testPet.Id);
+
+            Assert.IsAssignableFrom<Task>(result);
+        }
+
+        /// <summary>
+        /// Tests UpdatePet to check if result is type Task
+        /// </summary>
+
+        [Fact]
+        public void UpdatePet_TestIfType_Task()
+        {
+
+            var _petRepo = new PetRepository(_context.Object);
+            var result = _petRepo.UpdatePet(testPet);
+
+            Assert.IsAssignableFrom<Task>(result);
+        }
+
+        /// <summary>
+        /// Tests CheckQualification to check if result is type Task
+        /// </summary>
+
+        [Fact]
+        public void CheckQualifications_TestIfType_Task()
+        {
+
+            var _petRepo = new PetRepository(_context.Object);
+            var result = _petRepo.CheckQualification(testPet);
+
+            Assert.IsAssignableFrom<Task>(result);
+        }
+
+        /// <summary>
+        /// Tests DeletePet to check if result is type Task
+        /// </summary>
+
+        [Fact]
+        public void DeletePet_TestIfType_Task()
+        {
+
+            var _petRepo = new PetRepository(_context.Object);
+            var result = _petRepo.DeletePet(testPet);
+
+            Assert.IsAssignableFrom<Task>(result);
+        }
+
+        /// <summary>
+        /// Tests GetQuote to check if result is type Task
+        /// </summary>
+
+        [Fact]
+        public void GetQuote_TestIfType_Task()
+        {
+
+            var _petRepo = new PetRepository(_context.Object);
+            var result = _petRepo.GetQuote(testPet);
+
+            Assert.IsAssignableFrom<Task>(result);
+        }
 
     }
 }
