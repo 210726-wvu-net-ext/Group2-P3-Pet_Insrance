eb96b528bd6369b2e8b24e784df4945788c1b5ba
YYarytsk <iamfromukraine@gmail.com>
2021-10-12 12:59:59 -0400

    added purchase/payment validation

--- Client/src/app/pet/purchase/purchase.component.ts
+++ Client/src/app/pet/purchase/purchase.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from 'src/app/account/account.service';
 
 @Component({
   selector: 'app-purchase',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./purchase.component.css']
 })
 export class PurchaseComponent implements OnInit {
+  
 
-  constructor() { }
+  constructor(private service: AccountService, private formBuilder: FormBuilder) { }
 
+  form: FormGroup = new FormGroup(
+    {
+      username: new FormControl(''),
+      cardNumber: new FormControl(''),
+      expiry_month: new FormControl(''),
+      expiry_year: new FormControl(''),
+      password: new FormControl('')
+    });
+    
   ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      cardNumber: ['', Validators.required],
+      expiry_month: ['', Validators.required],
+      expiry_year: ['', Validators.required],
+      password: ['', Validators.required]
+    })
   }
 
 }
