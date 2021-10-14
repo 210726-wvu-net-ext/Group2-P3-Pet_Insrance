45831467f339a0113f1d0c3d88e2c3428b540bf2
Dekklin <collinhintzke@outlook.com>
2021-10-14 10:10:52 -0700

    added Cors on controller

--- WebAPI/WebAPI/Controllers/UsersController.cs
+++ WebAPI/WebAPI/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +12,7 @@ using WebAPI.Repositories.Interfaces;
 namespace WebAPI.Controllers
 {
     [Route("api/[controller]")]
-    [EnableCors("*")]
+    [EnableCors("AllowAll")]
     [ApiController]
     public class UsersController : ControllerBase
     {
